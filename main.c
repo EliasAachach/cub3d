2efce59704d88f5fa159849adc522a8304a75fb5
EliasAachach <eliasaachach@gmail.com>
2021-02-27 14:01:48 +0100

    first tests

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include <mlx.h>
 
-int		main(void)
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	void	*mlx;
-	void	*mlx_win;
-	
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
-	mlx_loop(mlx);
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int             main(void)
+{
+    int     xres = 720;
+    int     yres = 480;
+    void    *mlx;
+    void    *mlx_win;
+    t_data  img;
+
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, xres, yres, "Hello world!");
+ 
+    img.img = mlx_new_image(mlx, xres, yres);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+    my_mlx_pixel_put(&img, 5, 5, 0x00FFEC00);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 0);
+    mlx_loop(mlx);
 }
