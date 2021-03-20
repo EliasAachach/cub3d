8bbee60d878506d400caf0e4ae659f9c63c0c571
EliasAachach <eliasaachach@gmail.com>
2021-03-20 02:51:44 +0100

    parsing wip, using gnl

---
+++ srcs/parsing/parsing.c
@@ -0,0 +1,9 @@
+#include  <unistd.h>
+
+int     parsing()+;
+{
+    while (get_next_line(fd, line) == 1)
+        nbr_lines++;
+    //on compte le nombre de
+    //lignes de la map (tous les caracteres)
+}
