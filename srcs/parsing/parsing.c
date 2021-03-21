f8bbe8b6b3e0617bf7c50063df97977730521b0e
EliasAachach <eliasaachach@gmail.com>
2021-03-21 20:42:38 +0100

    parsing.c wip

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -1,9 +1,39 @@
+#include "get_next_line.h"
+#include "cub3d.h"
 #include  <unistd.h>
 
-int     parsing()+;
+char	**alloc_map(char **map, int nbr_lines, int line_len)
 {
+	int i;
+	i = 0;
+	while (i <= nbr_lines)
+	{
+		map[i] = ((char *)malloc(sizeof(char) * (line_len + 1)));
+		if (!map[i])
+			return (NULL);
+		i++;
+	}
+	retrun (map);
+}
+
+char	**find_map();
+{
+	char 	**map;
+	char 	*line;
+	int		fd;
+    int		nbr_lines;
+	int		line_len;
+
+	line = NULL;
+	fd = open ("cub3d.map", 0_RDONLY);
+    nbr_lines = 0;
+	line_len = 0;
     while (get_next_line(fd, line) == 1)
-        nbr_lines++;
-    //on compte le nombre de
-    //lignes de la map (tous les caracteres)
+    {
+		if (line_len < ft_strlen(line))
+			line_len = ft_strlen(line);
+		nbr_lines++;
+	}
+	map = alloc_map(map, nbr_lines, line_len);
+	return (map);
 }
